20942d3071c6bf39727f5662e1a634d19c9d6828
viktorburdeev <burdeevviktor01@gmail.com>
2024-11-12 19:18:17 +0300

    start

---
+++ src/main/java/com/example/AnimeInBG/AnimeInBgApplication.java
@@ -0,0 +1,14 @@
+package com.example.AnimeInBG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AnimeInBgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeInBgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AnimeInBG/AnimeInBgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AnimeInBG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeInBgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
